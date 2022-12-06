f501e8763a8f5e85587732a468230a6ee14aafc6
Joacim Wideving <jwideving@gmail.com>
2022-12-06 21:54:26 +0100

    day4 a and b

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -2,10 +2,33 @@ import { dataLoader } from '@/utils/data-loader'
 
 export const solveDay4A = async (path: string): Promise<number> => {
   const data = await dataLoader(path)
-  return 0
+  return data.reduce((acc, curr) => {
+    return (fullyContainsSection(curr) ? 1 : 0) + acc
+  }, 0)
+}
+
+export const fullyContainsSection = (input: string) => {
+  const [first, second] = input.split(',')
+  const [firstStart, firstEnd] = first.split('-').map((s) => Number(s))
+  const [secondStart, secondEnd] = second.split('-').map((s) => Number(s))
+
+  return (
+    (firstStart >= secondStart && firstEnd <= secondEnd) ||
+    (secondStart >= firstStart && secondEnd <= firstEnd)
+  )
+}
+
+export const doesntContainsSection = (input: string) => {
+  const [first, second] = input.split(',')
+  const [firstStart, firstEnd] = first.split('-').map((s) => Number(s))
+  const [secondStart, secondEnd] = second.split('-').map((s) => Number(s))
+
+  return firstEnd < secondStart || secondEnd < firstStart
 }
 
 export const solveDay4B = async (path: string): Promise<number> => {
   const data = await dataLoader(path)
-  return 0
+  return data.reduce((acc, curr) => {
+    return (doesntContainsSection(curr) ? 0 : 1) + acc
+  }, 0)
 }
--- src/tests/day4/day4.spec.ts
+++ src/tests/day4/day4.spec.ts
@@ -1,4 +1,9 @@
-import { solveDay4A, solveDay4B } from '@/day4'
+import {
+  doesntContainsSection,
+  fullyContainsSection,
+  solveDay4A,
+  solveDay4B,
+} from '@/day4'
 import { describe, expect, it } from 'vitest'
 
 const PATH = 'src/day4/test-input.txt'
@@ -8,10 +13,40 @@ describe('day4 A', () => {
   it('should be defined', async () => {
     expect(solveDay4A).toBeDefined()
   })
+
+  it('should return correct result from test data', async () => {
+    expect(await solveDay4A(PATH)).toEqual(2)
+  })
+
+  it('should return correct result from full data', async () => {
+    expect(await solveDay4A(FULL_PATH)).toEqual(305)
+  })
+
+  it('should fully contains section', () => {
+    expect(fullyContainsSection('1-3,2-3')).toBeTruthy()
+    expect(fullyContainsSection('21-40,22-40')).toBeTruthy()
+  })
 })
 
 describe('day4 B', () => {
   it('should be defined', async () => {
     expect(solveDay4B).toBeDefined()
+  })
+
+  it('doesnt contain section', () => {
+    expect(doesntContainsSection('2-4,6-8')).toBeTruthy()
+    expect(doesntContainsSection('2-3,4-5')).toBeTruthy()
+    expect(doesntContainsSection('5-7,7-9')).toBeFalsy()
+    expect(doesntContainsSection('2-8,3-7')).toBeFalsy()
+    expect(doesntContainsSection('6-6,4-6')).toBeFalsy()
+    expect(doesntContainsSection('2-6,4-8')).toBeFalsy()
+  })
+
+  it('should return correct result from test data', async () => {
+    expect(await solveDay4B(PATH)).toEqual(4)
+  })
+
+  it('should return correct result from full data', async () => {
+    expect(await solveDay4B(FULL_PATH)).toEqual(811)
   })
 })
