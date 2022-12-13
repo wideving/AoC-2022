b51ac95ecef177431f34205d5505643356d8d619
Joacim Wideving <jwideving@gmail.com>
2022-12-13 21:10:55 +0100

    added day9 skeleton

---
+++ src/day9/index.ts
@@ -0,0 +1,6 @@
+import { dataLoader } from '@/utils/data-loader'
+
+export const solveDay9A = async (path: string): Promise<number> => {
+  const data = await dataLoader(path)
+  return 0
+}
---
+++ src/tests/day9/day9.spec.ts
@@ -0,0 +1,11 @@
+import { solveDay9A } from '@/day9'
+import { describe, expect, it } from 'vitest'
+
+const PATH = 'src/day9/test-input.txt'
+const FULL_PATH = 'src/day9/full-input.txt'
+
+describe('day9 A', () => {
+  it('should be defined', () => {
+    expect(solveDay9A).toBeDefined()
+  })
+})
