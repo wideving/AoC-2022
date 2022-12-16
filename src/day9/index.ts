c19a0c2ffdf347c2bec1bd451b58239d1ffec4ca
Joacim Wideving <jwideving@gmail.com>
2022-12-17 00:57:12 +0100

    day9 a

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -1,6 +1,150 @@
 import { dataLoader } from '@/utils/data-loader'
 
+export interface Point {
+  x: number
+  y: number
+}
+
+export enum Direction {
+  LEFT,
+  UP,
+  RIGHT,
+  DOWN,
+}
+
+export interface Sum {
+  tailMovements: Point[]
+  wasAdjacent: boolean
+}
+
+export interface Move {
+  direction: Direction
+  steps: number
+}
+
 export const solveDay9A = async (path: string): Promise<number> => {
   const data = await dataLoader(path)
-  return 0
+  const moves = data.map(parseMove)
+
+  const headMovements = moves.reduce((acc, curr) => {
+    parseHeadMovement(curr, acc)
+    return acc
+  }, new Array<Point>({ x: 0, y: 0 }))
+
+  const tailMovements = getTailMovements(headMovements)
+
+  return getUniqueMovements(tailMovements).length
+}
+
+export const getUniqueMovements = (tailMovements: Point[]) => {
+  const uniqueMovements: Point[] = []
+
+  for (const move of tailMovements) {
+    if (
+      !uniqueMovements.find(
+        (uniqueMove) => uniqueMove.x === move.x && uniqueMove.y === move.y,
+      )
+    ) {
+      uniqueMovements.push(move)
+    }
+  }
+
+  return uniqueMovements
+}
+
+export const getTailMovements = (headMovements: Point[]): Point[] => {
+  const sum: Sum = headMovements.reduce(
+    (sum, headMove, index) => {
+      const lastTailMove = sum.tailMovements[sum.tailMovements.length - 1]
+
+      if (isAdjacent(lastTailMove, headMove)) {
+        sum.wasAdjacent = true
+        return sum
+      }
+
+      sum.tailMovements.push(headMovements[index - 1])
+      sum.wasAdjacent = false
+
+      return sum
+    },
+    {
+      tailMovements: new Array<Point>({ x: 0, y: 0 }),
+      wasAdjacent: false,
+    } as Sum,
+  )
+  return sum.tailMovements
+}
+
+export const isAdjacent = (tail: Point, head: Point) => {
+  const adjacentCoordinates = getAdjacentCoordinates(tail)
+  return (
+    adjacentCoordinates.filter((coordinates) => {
+      return head.x === coordinates.x && head.y === coordinates.y
+    }).length > 0
+  )
+}
+
+export const getAdjacentCoordinates = (point: Point): Point[] => {
+  return [
+    { x: point.x - 1, y: point.y }, // left
+    { x: point.x - 1, y: point.y + 1 }, // top left
+    { x: point.x, y: point.y + 1 }, // top
+    { x: point.x + 1, y: point.y + 1 }, // top right
+    { x: point.x + 1, y: point.y }, // right
+    { x: point.x + 1, y: point.y - 1 }, // bottom right
+    { x: point.x, y: point.y - 1 }, // bottom
+    { x: point.x - 1, y: point.y - 1 }, // bottom left
+    { x: point.x, y: point.y }, // center
+  ]
+}
+
+export const parseHeadMovement = (move: Move, coordinates: Point[]) => {
+  for (let i = 0; i < move.steps; i++) {
+    const last = coordinates[coordinates.length - 1] // get the last move
+
+    switch (move.direction) {
+      case Direction.UP:
+        coordinates.push({ x: last.x, y: last.y + 1 })
+        break
+      case Direction.DOWN:
+        coordinates.push({ x: last.x, y: last.y - 1 })
+        break
+      case Direction.LEFT:
+        coordinates.push({ x: last.x - 1, y: last.y })
+        break
+      case Direction.RIGHT:
+        coordinates.push({ x: last.x + 1, y: last.y })
+        break
+    }
+  }
+}
+
+export const parseMove = (row: string): Move => {
+  const [dir, steps] = row.split(' ')
+  let direction: Direction
+  switch (dir) {
+    case 'U': {
+      direction = Direction.UP
+      break
+    }
+    case 'L': {
+      direction = Direction.LEFT
+      break
+    }
+    case 'D': {
+      direction = Direction.DOWN
+      break
+    }
+    case 'R': {
+      direction = Direction.RIGHT
+      break
+    }
+    default: {
+      throw Error('Not possible')
+    }
+  }
+  return {
+    direction,
+    steps: Number(steps),
+  }
 }
--- src/tests/day9/day9.spec.ts
+++ src/tests/day9/day9.spec.ts
@@ -1,4 +1,13 @@
-import { solveDay9A } from '@/day9'
+import {
+  Direction,
+  getAdjacentCoordinates,
+  getTailMovements,
+  isAdjacent,
+  parseHeadMovement,
+  parseMove,
+  Point,
+  solveDay9A,
+} from '@/day9'
 import { describe, expect, it } from 'vitest'
 
 const PATH = 'src/day9/test-input.txt'
@@ -7,5 +16,64 @@ const FULL_PATH = 'src/day9/full-input.txt'
 describe('day9 A', () => {
   it('should be defined', () => {
     expect(solveDay9A).toBeDefined()
+  })
+
+  it('should return correct result from test data', async () => {
+    expect(await solveDay9A(PATH)).toEqual(13)
+  })
+
+  it('should return correct result from full data', async () => {
+    expect(await solveDay9A(FULL_PATH)).toEqual(5874)
+  })
+
+  it('should return true if adjacent false otherwise', () => {
+    expect(isAdjacent({ x: 0, y: 0 }, { x: 0, y: 1 })).toBeTruthy()
+    expect(isAdjacent({ x: 0, y: 0 }, { x: 0, y: 2 })).toBeFalsy()
+    expect(isAdjacent({ x: 0, y: 0 }, { x: -1, y: 1 })).toBeTruthy()
+  })
+
+  it('should return adjacent coordinates', () => {
+    const adjacentCoordinates = getAdjacentCoordinates({ x: 0, y: 0 })
+    expect(adjacentCoordinates).toStrictEqual([
+      { x: -1, y: 0 }, // left
+      { x: -1, y: 1 }, // top left
+      { x: 0, y: 1 }, // top
+      { x: 1, y: 1 }, // top right
+      { x: 1, y: 0 }, // right
+      { x: 1, y: -1 }, // bottom right
+      { x: 0, y: -1 }, // bottom
+      { x: -1, y: -1 }, // bottom left
+      { x: 0, y: 0 }, // center
+    ])
+  })
+
+  it('should parse a row to a correct move', () => {
+    expect(parseMove('R 5')).toEqual({ direction: Direction.RIGHT, steps: 5 })
+    expect(parseMove('U 3')).toEqual({ direction: Direction.UP, steps: 3 })
+    expect(parseMove('L 1')).toEqual({ direction: Direction.LEFT, steps: 1 })
+    expect(parseMove('D 4')).toEqual({ direction: Direction.DOWN, steps: 4 })
+  })
+
+  it('should make a series of coordinates of a move', () => {
+    const move1 = parseMove('R 3')
+    const initialCoordinates: Point[] = [{ x: 0, y: 0 }]
+    parseHeadMovement(move1, initialCoordinates)
+    expect(initialCoordinates).toStrictEqual([
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 3, y: 0 },
+    ])
+
+    const move2 = parseMove('U 2')
+    parseHeadMovement(move2, initialCoordinates)
+    expect(initialCoordinates).toStrictEqual([
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 3, y: 0 },
+      { x: 3, y: 1 },
+      { x: 3, y: 2 },
+    ])
   })
 })
