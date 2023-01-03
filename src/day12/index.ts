174368fa83c4a8d9999ae126ecdad269c281d68c
Joacim Wideving <jwideving@gmail.com>
2023-01-03 22:05:44 +0100

    day12 b

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -1,6 +1,11 @@
 import { dataLoader } from '@/utils/data-loader'
+export interface Node {
+  row: number
+  col: number
+  parent?: Node
+}
 
-export const solveDay12A = async (path: string) => {
+export const solveDay12A = async (path: string): Promise<number> => {
   const data = await dataLoader(path)
 
   const { grid, start, end } = parseToNumbers(data)
@@ -15,15 +20,42 @@ export const solveDay12A = async (path: string) => {
     )
     steps++
     node = next
-    console.log(node)
   }
   return steps
 }
 
-export interface Node {
-  row: number
-  col: number
-  parent?: Node
+export const solveDay12B = async (path: string): Promise<number> => {
+  const data = await dataLoader(path)
+  const { grid, starts, end } = parseToNumbersB(data)
+
+  const completedPaths: Node[][] = []
+
+  for (const start of starts) {
+    completedPaths.push(bfs(grid, start, end).reverse())
+  }
+
+  const steps = completedPaths
+    .map((nodes) => {
+      return countSteps(nodes)
+    })
+    .filter((value) => value !== 0)
+    .sort()
+  console.log(steps)
+  return steps[0]
+}
+
+export const countSteps = (nodes: Node[]) => {
+  // revisit nodes and return count
+  let node: Node | undefined = nodes[0]
+  let steps = 0
+  while (node?.parent) {
+    const next = nodes.find(
+      (n) => n.row === node?.parent?.row && n.col === node.parent?.col,
+    )
+    steps++
+    node = next
+  }
+  return steps
 }
 
 export const bfs = (grid: number[][], start: Node, end: Node): Node[] => {
@@ -33,11 +65,16 @@ export const bfs = (grid: number[][], start: Node, end: Node): Node[] => {
   // create a set for storing visited nodes
   const visited = new Set<string>()
 
+  // store the path, addding a parent to the node allows
+  // us to backtrack and count the number of steps
   const path: Node[] = []
 
   // enqueue the starting node
   queue.push(start)
+
+  // push to path
   path.push(start)
+
   visited.add(`${start.row},${start.col}`)
   // arrays for storing the row and column offsets
   const rowOffsets = [-1, 0, 1, 0]
@@ -85,6 +122,36 @@ export const bfs = (grid: number[][], start: Node, end: Node): Node[] => {
 
   // if the goal was not reached, return empty array
   return []
+}
+
+export const parseToNumbersB = (
+  data: string[],
+): { grid: number[][]; starts: Node[]; end: Node } => {
+  const startingPositions: Node[] = []
+  let endPosition: Node
+
+  return {
+    grid: data.map((row, rowIndex) => {
+      return row.split('').map((char, columnIndex) => {
+        if (char === 'S') {
+          startingPositions.push({ row: rowIndex, col: columnIndex })
+          return 'a'.charCodeAt(0) - 97
+        }
+
+        if (char === 'a') {
+          startingPositions.push({ row: rowIndex, col: columnIndex })
+        }
+
+        if (char === 'E') {
+          endPosition = { row: rowIndex, col: columnIndex }
+          return 'z'.charCodeAt(0) - 97
+        }
+        return Number(char.charCodeAt(0) - 97)
+      })
+    }),
+    starts: startingPositions,
+    end: endPosition!,
+  }
 }
 
 export const parseToNumbers = (
--- src/tests/day12/day12.spec.ts
+++ src/tests/day12/day12.spec.ts
@@ -1,4 +1,9 @@
-import { solveDay12A } from '@/day12'
+import {
+  parseToNumbers,
+  parseToNumbersB,
+  solveDay12A,
+  solveDay12B,
+} from '@/day12'
 import { describe, expect, it } from 'vitest'
 
 const PATH = 'src/day12/test-input.txt'
@@ -9,7 +14,36 @@ describe('day12 A', () => {
     expect(await solveDay12A(PATH)).toEqual(31)
   })
 
+  it('should parse to numbers', () => {
+    expect(parseToNumbers(['SaE'])).toStrictEqual({
+      grid: [[0, 0, 25]],
+      start: { row: 0, col: 0 },
+      end: { row: 0, col: 2 },
+    })
+  })
+
   it('should correct result from full data', async () => {
     expect(await solveDay12A(FULL_PATH)).toEqual(426)
+  })
+})
+
+describe('day12 B', () => {
+  it('should correct result from test data', async () => {
+    expect(await solveDay12B(PATH)).toEqual(29)
+  })
+
+  it('should correct result from full data', async () => {
+    expect(await solveDay12B(FULL_PATH)).toEqual(419)
+  })
+
+  it('should return the correct starting points', () => {
+    expect(parseToNumbersB(['SaE'])).toStrictEqual({
+      grid: [[0, 0, 25]],
+      starts: [
+        { row: 0, col: 0 },
+        { row: 0, col: 1 },
+      ],
+      end: { row: 0, col: 2 },
+    })
   })
 })
